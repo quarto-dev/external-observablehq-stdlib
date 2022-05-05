31e98afa3bc696c560206c6bba6737841ac5596d
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-05-05 12:57:13 -0700

    update copyright

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import node from "rollup-plugin-node-resolve";
 import {terser} from "rollup-plugin-terser";
 import * as meta from "./package.json";
 
-const copyright = `// @observablehq/stdlib v${meta.version} Copyright ${(new Date).getFullYear()} Observable, Inc.`;
+const copyright = `// @quarto/external-observablehq-stdlib ${meta.version} Fork changes are Copyright ${(new Date).getFullYear()} RStudio, PBC.\n // @observablehq/stdlib v${meta.version} Copyright ${(new Date).getFullYear()} Observable, Inc.`;
 
 export default [
   {
